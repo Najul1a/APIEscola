794903db611405f92422a569c662cb08a7799d71
Ana Geremias <anageremiias@gmail.com>
2025-04-15 16:58:19 -0300

    Autentificação

--- APIEscola/Data/APIEscolaContext.cs
+++ APIEscola/Data/APIEscolaContext.cs
@@ -1,4 +1,4 @@
-﻿// Codigo do Arquivo 
+﻿using APIEscola.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,21 +6,30 @@ namespace APIEscola.Data
 {
     public class APIEscolaContext : IdentityDbContext
     {
+        //Construtor que recebe as opções de configuração do DbContext
         public APIEscolaContext(DbContextOptions<APIEscolaContext> options) : base(options)
         {
         }
+        //Propriedade DbSet para cada tabela
+        public DbSet<Aluno> Alunos { get; set; } //Tabela de alunos
+        public DbSet<Curso> Cursos { get; set; } //Tabela de cursos
+        public DbSet<Turma> Turmas { get; set; } //Tabela de cursos
 
-        //Propriedade DbSet para acessar as tabelas
+        public DbSet<Matricula> Matriculas { get; set; } //Tabela de cursos
 
-        //Sobrecarga de método OnModelCreating para configurar o modelo a partir da IdentityDbContext
+        //Sobrecarga do método OnModelCreating para configurar o modelo a partir da IdentityDbContext5
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //chamado o método OnModelCreating da classe base para criação das tabelas padões 
+            //Chama o método OnModelCreating da classe base para a criação das tabelas padrão
             base.OnModelCreating(modelBuilder);
-            //Configurar a criação de tabelas adicionais aqui 
+
+            //Configurar a criação de tabelas adicionais aqui
+            modelBuilder.Entity<Aluno>().ToTable("Alunos"); //Configura o nome da tabela Alunos
+            modelBuilder.Entity<Curso>().ToTable("Cursos"); //Configura o nome da tabela Alunos
+            modelBuilder.Entity<Turma>().ToTable("Turmas"); //Configura o nome da tabela Alunos
+            modelBuilder.Entity<Matricula>().ToTable("Matriculas"); //Configura o nome da tabela Alunos
         }
     }
 }
 
 
-// =========================================================================================
---
+++ APIEscola/Migrations/20250415190806_Matricula.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIEscola.Migrations
+{
+    /// <inheritdoc />
+    public partial class Matricula : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Turmas",
+                columns: table => new
+                {
+                    TurmaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataInicio = table.Column<DateOnly>(type: "date", nullable: false),
+                    DataFim = table.Column<DateOnly>(type: "date", nullable: false),
+                    Sigla = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    CursoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turmas", x => x.TurmaId);
+                    table.ForeignKey(
+                        name: "FK_Turmas_Cursos_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Cursos",
+                        principalColumn: "CursoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matriculas",
+                columns: table => new
+                {
+                    MatriculaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AlunoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TurmaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataMatricula = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matriculas", x => x.MatriculaId);
+                    table.ForeignKey(
+                        name: "FK_Matriculas_Alunos_AlunoId",
+                        column: x => x.AlunoId,
+                        principalTable: "Alunos",
+                        principalColumn: "AlunoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matriculas_Turmas_TurmaId",
+                        column: x => x.TurmaId,
+                        principalTable: "Turmas",
+                        principalColumn: "TurmaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matriculas_AlunoId",
+                table: "Matriculas",
+                column: "AlunoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matriculas_TurmaId",
+                table: "Matriculas",
+                column: "TurmaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Turmas_CursoId",
+                table: "Turmas",
+                column: "CursoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matriculas");
+
+            migrationBuilder.DropTable(
+                name: "Turmas");
+        }
+    }
+}
